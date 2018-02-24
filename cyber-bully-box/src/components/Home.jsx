a62a2e3b9841e292cae599ebc24ddd79c7b08be8
Daniel Scott <daniel@mydeveloperjourney.com>
2018-02-23 22:08:25 -0600

    adds home component

--- cyber-bully-box/src/components/Home.jsx
+++ cyber-bully-box/src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import '../css/Home.css'
 
 class Home extends Component{
   render(){
     return(
-      <div>
-        <h1>This is Home</h1>
+      <div className='Home'>
+        <h1 className='title'>Welcome to Upstandr</h1>
       </div>
     )
   }
--- cyber-bully-box/src/config/routes.js
+++ cyber-bully-box/src/config/routes.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Switch, Route} from 'react-router-dom';
-import Base from '../components/Base';
-import Header from '../components/Base';
 import Login from '../components/Login';
 import Home from '../components/Home'
 
