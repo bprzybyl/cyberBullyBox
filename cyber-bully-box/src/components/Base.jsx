2465ec1fb6bb41bee69d3b04760e7ad5a1668a28
Daniel Scott <daniel@mydeveloperjourney.com>
2018-02-23 19:50:49 -0600

    adds base, footer, and header

--- cyber-bully-box/src/components/Base.jsx
+++ cyber-bully-box/src/components/Base.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from 'react'
-
+import Header from './Header';
+import Footer from './Footer';
 class Base extends Component{
   render(){
     return(
       <div>
+        <Header />
         <div>Hello from the Base component</div>
+        <Footer />
       </div>
     )
   }
