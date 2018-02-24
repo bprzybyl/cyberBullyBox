cb8e8b0c7447605fcab95c638233e65599758740
Daniel Scott <daniel@mydeveloperjourney.com>
2018-02-24 09:52:28 -0600

    adds additional styling for the about section

--- cyber-bully-box/src/components/About.jsx
+++ cyber-bully-box/src/components/About.jsx
@@ -3,23 +3,30 @@ import React, {Component} from 'react';
 class About extends Component{
   render(){
     return(
-      <div>
-        <p>THE UPSTANDR MISSION STATEMENT:
+      <div className="about">
+      <div className="row">
+        <div className="col-md-3"></div>
+        <div className="col-md-6 text-center">
+          <h1 className="mt-5">THE UPSTANDR MISSION STATEMENT</h1>
+          <p className="lead text-center">In the society of our youth, it has become an alarming statistic of cyberbullying and associated suicide deaths as a result.
+            Clearly, this is an issue for our community and for parents who have children who are being bullied on various social media networks.
+            It is our goal and passion to drastically reduce and thereby virtually eliminate cyberbullying from schools, social networks and in person.
+            This is a lofty goal, we know, but with consistent and persistent effort and work ethic we truly believe it will come to fruition. We are Upstandrs and we challenge you to be an Upstandr too—because being a bystander is no longer an option, when there are lives to be saved.  Our web application (app) and ultimately our mobile device interface will allow a concerned student, parent and of course, a student being bullied to notify the appropriate entities anonymously (if they choose), get the appropriate guidance and help they want, and also offers recovery resources and programs for the bully, as well.
+            Because truly ending the crime of cyberbullying means going to the source of the issue and healing their woulds and changing their behaviors too.
+            “Because hurt people, hurt people.” We are here to create a humanity in future generations that cultivates peace (and peace of mind), love, humility, kindness, fun and togetherness for us all; which ultimately builds a world that we all love being part of everyday.
+            Please join us in this battle to take down the “crown” of cyberbullying and placing it where it really belongs—in the depths of the sea.
+            Thank you for being an Upstandr, we couldn’t do this without you.</p>
 
-In the society of our youth, it has become an alarming statistic of cyberbullying and associated suicide deaths as a result.  Clearly, this is an issue for our community and for parents who have children who are being bullied on various social media networks.
-
-It is our goal and passion to drastically reduce and thereby virtually eliminate cyberbullying from schools, social networks and in person.  This is a lofty goal, we know, but with consistent and persistent effort and work ethic we truly believe it will come to fruition.
-
-We call ourselves Upstandrs and we challenge you to be an Upstandr too—because being a bystander is no longer an option, when there are lives to be saved.  Our web application (app) and ultimately our mobile device interface will allow a concerned student, parent and of course, a student being bullied to notify the appropriate entities anonymously (if they choose), get the appropriate guidance and help they want, and also offers recovery resources and programs for the bully, as well.  Because truly ending the crime of cyberbullying means going to the source of the issue and healing their woulds and changing their behaviors too. “Because hurt people, hurt people.”
-
-We are here to create a humanity in future generations that cultivates peace (and peace of mind), love, humility, kindness, fun and togetherness for us all; which ultimately builds a world that we all love being part of everyday.
-
-Please join us in this battle to take down the “crown” of cyberbullying and placing it where it really belongs—in the depths of the sea.
-
-Thank you for being an Upstandr, we couldn’t do this without you.
-
-        The CyberBullyBox Team</p>
+          <h3>The Upstandr Team</h3>
+          <ul className="list-unstyled">
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div className="col-md-3"></div>
       </div>
+    </div>
+
     )
   }
 }
