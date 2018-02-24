d2697105f69e6684ab12a839f7d76b136f478716
Daniel Scott <daniel@mydeveloperjourney.com>
2018-02-24 00:43:08 -0600

    adds form detail

--- cyber-bully-box/src/components/Register.jsx
+++ cyber-bully-box/src/components/Register.jsx
@@ -3,112 +3,112 @@ import React, {Component} from 'react';
 class Register extends Component{
   render(){
     return(
-      <div class="row">
-  <div class="col-md-6">
-    <div class="container">
-      <p class="h2">New Student Registration:</p>
+  <div className="row">
+  <div className="col-md-6">
+    <div className="container">
+      <p className="h2">New Student Registration:</p>
       <form>
-        <div class="row">
-          <div class="col-3">
-            <p class="h5 text-right">Email:</p>
+        <div className="row">
+          <div className="col-3">
+            <p className="h5 text-right">Email:</p>
           </div>
-          <div class="col-9 form-group">
-            <input type="email" class="form-control" id="inputStudentEmail" aria-describedby="StudentEmail" placeholder="Enter Email">
-          </div>
-        </div>
-
-        <div class="row form-group">
-          <div class="col-3">
-            <p class="h5 text-right">First Name:</p>
-          </div>
-          <div class="col-9">
-            <input type="text" class="form-control" id="inputStudentFirstName" aria-describedby="StudentFirstName" placeholder="Enter First Name">
+          <div className="col-9 form-group">
+            <input type="email" className="form-control" id="inputStudentEmail" aria-describedby="StudentEmail" placeholder="Enter Email"/>
           </div>
         </div>
 
-        <div class="row form-group">
-          <div class="col-md-3">
-            <p class="h5 text-right">Last Name:</p>
+        <div className="row form-group">
+          <div className="col-3">
+            <p className="h5 text-right">First Name:</p>
           </div>
-          <div class="col-md-9">
-            <input type="text" class="form-control" id="inputStudentLastName" aria-describedby="StudentLastName" placeholder="Enter Last Name">
-          </div>
-        </div>
-
-        <div class="row form-group">
-          <div class="col-3">
-            <p class="h5 text-right">School ID:</p>
-          </div>
-          <div class="col-9">
-            <input type="text" class="form-control" id="inputStudentSchoolID" aria-describedby="StudentSchoolID" placeholder="Enter School ID">
+          <div className="col-9">
+            <input type="text" className="form-control" id="inputStudentFirstName" aria-describedby="StudentFirstName" placeholder="Enter First Name"/>
           </div>
         </div>
 
-        <div class="row form-group">
-          <div class="col-3">
+        <div className="row form-group">
+          <div className="col-md-3">
+            <p className="h5 text-right">Last Name:</p>
           </div>
-          <div class="col-9">
-            <button type="submit" class="btn-primary btn" id="submitStudentButton">Submit</button>
+          <div className="col-md-9">
+            <input type="text" className="form-control" id="inputStudentLastName" aria-describedby="StudentLastName" placeholder="Enter Last Name"/>
+          </div>
+        </div>
+
+        <div className="row form-group">
+          <div className="col-3">
+            <p className="h5 text-right">School ID:</p>
+          </div>
+          <div className="col-9">
+            <input type="text" className="form-control" id="inputStudentSchoolID" aria-describedby="StudentSchoolID" placeholder="Enter School ID"/>
+          </div>
+        </div>
+
+        <div className="row form-group">
+          <div className="col-3">
+          </div>
+          <div className="col-9">
+            <button type="submit" className="btn-primary btn" id="submitStudentButton">Submit</button>
           </div>
         </div>
 
       </form>
     </div>
   </div>
-  <div class="col-md-6">
-    <div class="container">
-      <p class="h2">New Parent Registration:</p>
+  <div className="col-md-6">
+    <div className="container">
+      <p className="h2">New Parent Registration:</p>
       <form>
-        <div class="row">
-          <div class="col-3">
-            <p class="h5 text-right">Email:</p>
+        <div className="row">
+          <div className="col-3">
+            <p className="h5 text-right">Email:</p>
           </div>
-          <div class="col-9 form-group">
-            <input type="email" class="form-control" id="inputParentEmail" aria-describedby="ParentEmail" placeholder="Enter Email">
-          </div>
-        </div>
-
-        <div class="row form-group">
-          <div class="col-3">
-            <p class="h5 text-right">First Name:</p>
-          </div>
-          <div class="col-9">
-            <input type="text" class="form-control" id="inputParentFirstName" aria-describedby="ParentFirstName" placeholder="Enter First Name">
+          <div className="col-9 form-group">
+            <input type="email" className="form-control" id="inputParentEmail" aria-describedby="ParentEmail" placeholder="Enter Email"/>
           </div>
         </div>
 
-        <div class="row form-group">
-          <div class="col-md-3">
-            <p class="h5 text-right">Last Name:</p>
+        <div className="row form-group">
+          <div className="col-3">
+            <p className="h5 text-right">First Name:</p>
           </div>
-          <div class="col-md-9">
-            <input type="text" class="form-control" id="inputParentLastName" aria-describedby="ParentLastName" placeholder="Enter Last Name">
-          </div>
-        </div>
-
-        <div class="row form-group">
-          <div class="col-3">
-            <p class="h5 text-right">Access Code:</p>
-          </div>
-          <div class="col-9">
-            <input type="text" class="form-control" id="inputParentAccessCode" aria-describedby="ParentAccessCode" placeholder="Enter Access Code">
+          <div className="col-9">
+            <input type="text" className="form-control" id="inputParentFirstName" aria-describedby="ParentFirstName" placeholder="Enter First Name"/>
           </div>
         </div>
 
-        <div class="row form-group">
-          <div class="col-3">
-            <p class="h5 text-right">Children:</p>
+        <div className="row form-group">
+          <div className="col-md-3">
+            <p className="h5 text-right">Last Name:</p>
           </div>
-          <div class="col-9">
-            <input type="text" class="form-control" id="inputParentSchoolID" aria-describedby="ParentSchoolID" placeholder="Enter School ID">
+          <div className="col-md-9">
+            <input type="text" className="form-control" id="inputParentLastName" aria-describedby="ParentLastName" placeholder="Enter Last Name"/>
           </div>
         </div>
 
-        <div class="row form-group">
-          <div class="col-3">
+        <div className="row form-group">
+          <div className="col-3">
+            <p className="h5 text-right">Access Code:</p>
           </div>
-          <div class="col-9">
-            <button type="submit" class="btn-primary btn" id="submitParentButton">Submit</button>
+          <div className="col-9">
+            <input type="text" className="form-control" id="inputParentAccessCode" aria-describedby="ParentAccessCode" placeholder="Enter Access Code"/>
+          </div>
+        </div>
+
+        <div className="row form-group">
+          <div className="col-3">
+            <p className="h5 text-right">Children:</p>
+          </div>
+          <div className="col-9">
+            <input type="text" className="form-control" id="inputParentSchoolID" aria-describedby="ParentSchoolID" placeholder="Enter School ID"/>
+          </div>
+        </div>
+
+        <div className="row form-group">
+          <div className="col-3">
+          </div>
+          <div className="col-9">
+            <button type="submit" className="btn-primary btn" id="submitParentButton">Submit</button>
           </div>
         </div>
 
