30a9ba0d516276a2d2f6d116eaae7db06578276c
Daniel Scott <daniel@mydeveloperjourney.com>
2018-02-23 20:44:19 -0600

    adds browser router and additional component

---
+++ cyber-bully-box/src/config/routes.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Switch, Route} from 'react-router-dom';
+import Base from '../components/Base';
+import Header from '../components/Base';
+import Login from '../components/login';
--- cyber-bully-box/src/index.js
+++ cyber-bully-box/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Base from './components/Base';
+import { BrowserRouter } from 'react-router-dom'
 
 
-ReactDOM.render(<Base />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+      <Base />
+  </BrowserRouter>,
+  document.getElementById('root'));
