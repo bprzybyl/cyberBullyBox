befc3dadd76c1f6280d483589d4a7167ede2c963
Daniel Scott <daniel@mydeveloperjourney.com>
2018-02-23 21:29:21 -0600

    adds additional css styling

--- cyber-bully-box/src/components/Header.jsx
+++ cyber-bully-box/src/components/Header.jsx
@@ -4,31 +4,31 @@ import {Link} from 'react-router-dom';
 class Header extends Component{
   render(){
     return(
-      <div>
+      <div className='header'>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-          <div className="container">
-            <a className="navbar-brand" href="#">Upstander</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-        <ul className="navbar-nav ml-auto">
-        <li className="nav-item active">
-          <Link className="nav-link" to={'/'}>Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to={'/Login'}>Logon</Link>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Services</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        <div className="container">
+        <a className="navbar-brand" href="#">Upstander</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item active">
+                  <Link className="nav-link" to={'/'}>Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/Login'}>Logon</Link>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Services</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+          </div>
+      </div>
+    </nav>
 
       </div>
     )
