1dfd1cf69265df5cd2a738dc97071363b07b49a3
Daniel Scott <daniel@mydeveloperjourney.com>
2018-02-23 19:31:07 -0600

    adds base component

---
+++ cyber-bully-box/src/components/Base.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Base extends Component{
+  render(){
+    return(
+      <div>
+        <div>Hello from the Base component</div>
+      </div>
+    )
+  }
+}
+
+export default Base;
--- cyber-bully-box/src/index.js
+++ cyber-bully-box/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Base from './components/Base';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(<Base />, document.getElementById('root'));
