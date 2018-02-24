2ff20029d47e3636450fc2afc94ef58f5d14b19b
bprzybyl <brooks.przybylek@gmail.com>
2018-02-24 13:36:42 -0600

    adds admin links

--- cyber-bully-box/src/components/AdminDashboard.jsx
+++ cyber-bully-box/src/components/AdminDashboard.jsx
@@ -18,10 +18,21 @@ class AdminDashboard extends Component{
                 <h1>Hello Robert</h1>
                 <h4>Your on the Dashboard</h4>
                 <h4><small> Here you can see what students are saying about their experience today</small></h4>
-                <ul>
-                  <li><a href="">Student Parents Directory</a></li>
-                </ul>
-            </div>
+                  <ul>
+                    <li><a href="">Student-Guardian Directory</a></li>
+                    <p>Quickly finds a Student's Legal Guardian</p>
+                    <li><a href="">Safety Officer Contact Information</a></li>
+                    <p>Contact Info for School Safety Offical</p>
+                    <li><a href="">Crisis Response Protocol</a></li>
+                    <p>Steps to take when trouble brewing</p>
+                    <li><a href="">Violence Prevention Actions</a></li>
+                    <p>Guide for using digital evidence to prevent altercations</p>
+                    <li><a href="">Legislative Requirements</a></li>
+                    <p>Resources explaining legislative expectations</p>
+                    <li><a href="">Anti-Bullying Materials for Guardians</a></li>
+                    <p>Digital Materials for Distribution to Parents</p>
+                  </ul>
+                </div>
             <div className="col-md-7">
               <Feed />
            </div>
